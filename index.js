f8336c5023c5d6ba9d4737c9bb98112b44b63155
Kazi Sanam <sanamish@gmail.com>
2018-01-09 04:22:53 +0000

    Done.

--- index.js
+++ index.js
@@ -51,15 +51,15 @@ describe('index.js', function() {
 
     it('charges 2 cents per foot when total feet travelled is between 400 and 2000 in example below is 528', function() {
       // total feet travelled is 528 -> 10.56
-      expect(calculatesFarePrice(34, 32)).to.equal(10.56);
+      expect(calculatesFarePrice(400, 2000)).to.equal(10.56);
     });
 
     it('charges 25 dollars for a distance over 2000 feet', function() {
-      expect(calculatesFarePrice(50, 58)).to.equal(25);
+      expect(calculatesFarePrice(2000,2500)).to.equal(25);
     });
 
     it('does not allow rides over 2500 feet', function() {
-      expect(calculatesFarePrice(34, 24)).to.equal('cannot travel that far');
+      expect(calculatesFarePrice(2500)).to.greaterthan('cannot travel that far');
     });
   });
 });
