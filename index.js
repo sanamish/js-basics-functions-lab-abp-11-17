7bf47fa8a0441685e57c8eade0b4999d3ec180e1
Kazi Sanam <sanamish@gmail.com>
2018-01-10 04:02:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
-function distanceFromHqInBlocks (43, 42) {
+function distanceFromHqInBlocks (distance) {
   return 43-42
 }
+
+function calculatesFarePrice(distance){
+  if (distance <== 400){
+    return '0'
+  }
+  else if (distance <400 && distance >2000){
+    return 'charges 2 cents per foot when total feet travelled is between 400 and 2000 in example below is 528'
+  }
+}
+
+  describe('calculatesFarePrice(start, destination)', function() {
+    it('gives customers a free sample', function() {
+      expect(calculatesFarePrice(43, 44)).to.equal(0);
+    });
+
+    it('charges 2 cents per foot when total feet travelled is between 400 and 2000 in example below is 528', function() {
+      // total feet travelled is 528 -> 10.56
+      expect(calculatesFarePrice(34, 32)).to.equal(10.56);
+    });
+
+    it('charges 25 dollars for a distance over 2000 feet', function() {
+      expect(calculatesFarePrice(50, 58)).to.equal(25);
+    });
+
+    it('does not allow rides over 2500 feet', function() {
+      expect(calculatesFarePrice(34, 24)).to.equal('cannot travel that far');
+    });
+  });
+});
+
